a612d47f37f250a119fd16b4cd3de83c2498af49
1n0r1 <80285371+1n0r1@users.noreply.github.com>
2021-07-01 07:49:06 +0700

    Added character and shooting function

--- game.py
+++ game.py
@@ -1,24 +1,39 @@
 import sys, pygame, random
 
+from pygame import mouse
+from pygame.constants import BUTTON_LEFT, MOUSEBUTTONDOWN
+from pygame.key import get_pressed
 
 pygame.init()
 size = width, height = 1366, 768
 screen = pygame.display.set_mode(size)
 speed = [0,0]
+shooting = False
+last_shoot = pygame.time.get_ticks()
+
 
 
 class MainCharacter(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        # image
-        # rect
         self.image = pygame.image.load("mc.png")
-
         self.rect = self.image.get_rect()
         self.rect.center = (250,250)
 
-
-
+class Bullet(pygame.sprite.Sprite):
+    facing = [0,0]
+    def __init__(self, color, pos, face):
+        super().__init__() 
+        self.image = pygame.image.load("bullet.png")
+        self.image = pygame.transform.scale(self.image, (25, 25))
+        self.rect = self.image.get_rect()
+        colorImage = pygame.Surface(self.image.get_size()).convert_alpha()
+        colorImage.fill(color)
+        self.image.blit(colorImage, (0,0), special_flags = pygame.BLEND_MULT)
+        self.rect.center = pos
+        self.facing = face
+    def update(self):
+        self.rect = self.rect.move(self.facing)
 def handle_movement():
     key = pygame.key.get_pressed()
     if (key[pygame.K_a]):
@@ -43,18 +58,49 @@ def handle_movement():
         speed[1] += 1
     main.rect = main.rect.move(speed)
 
+def handle_event():
+    for event in pygame.event.get():
+        global shooting
+        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            shooting = True
+        if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+            shooting = False
+
+def shoot():
+    t = pygame.time.get_ticks()
+    global last_shoot
+    if (t - last_shoot > 50):
+        face = [0.0,0.0]
+        face[0] = pygame.mouse.get_pos()[0] - main.rect.center[0]
+        face[1] = pygame.mouse.get_pos()[1] - main.rect.center[1]
+        f =[0.0,0.0]
+        f[0] = face[0]*20 / (face[0]**2 + face[1]**2)**(1/2)
+        f[1] = face[1]*20 / (face[0]**2 + face[1]**2)**(1/2)
+        bullet = Bullet(pygame.Color(255,0,0,255), main.rect.center, f)
+        bullet_sprites.add(bullet)
+        last_shoot = pygame.time.get_ticks()
+
+def refresh():
+    screen.fill((255, 255, 255))
+    main_sprite.draw(screen)
+    bullet_sprites.draw(screen)
+    bullet_sprites.update()
+    pygame.display.flip()
+
 
 main = MainCharacter()
 main_sprite = pygame.sprite.Group()
 main_sprite.add(main)
 
-clock = pygame.time.Clock()
+bullet_sprites = pygame.sprite.Group()
+
 while 1:
-    clock.tick(60)
-    handle_movement()
+    pygame.time.Clock().tick(60)
+
+    if (shooting == True):
+        shoot()
     
-    screen.fill((255, 255, 255))
-    main_sprite.draw(screen)
-    pygame.display.flip()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+    refresh()
+    handle_event()
+    handle_movement()
